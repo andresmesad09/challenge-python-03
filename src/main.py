3c41e13b7927b5d3289d4180c346659302df4bd6
Andres Mesa <andresf.mesad@gmail.com>
2020-07-14 19:39:55 -0500

    Texto encontrado

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
 # Resolve the problem!!
+import re
 
 
 def run():
     # Start coding here
-
+    letters = []
+    with open('encoded.txt', 'r', encoding='utf-8') as f:
+    # code
+        
+         # reading each line     
+        for line in f: 
+    
+            # reading each word         
+            for word in line.split(): 
+    
+                # agg each lower case letter in letters            
+                matches = re.findall(r'[a-z]+', word)
+                letters.append(matches)  
+    
+    
+    hiden_word = ''.join(str(letter) for letter in letters)
+    print(f'El mensaje oculto es {hiden_word}')
 
 if __name__ == '__main__':
     run()
